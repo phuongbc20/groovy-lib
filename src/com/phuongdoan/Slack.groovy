cd8b31b44c73afbc52a347bf336c06da3f34bfde
admin <admin@example.com>
2023-05-08 17:57:55 +0700

    Init

--- src/com/phuongdoan/Slack.groovy
+++ src/com/phuongdoan/Slack.groovy
@@ -15,7 +15,7 @@ class Slack implements Serializable {
             String date = script.sh(returnStdout: true, script: 'TZ=":Asia/Ho_Chi_Minh" date "+%Y-%m-%d %T"').trim()
             String message = "{'attachments': [ { 'text': 'URL', 'color': #4BB543, 'title': 'title' } ] }"
 
-            script.sh("curl -H \"Content-Type:application/json\" -X POST --data \"abc\" ${slackHook}")
+            script.sh("curl -H \"Content-Type:application/json\" -X POST --data \"${message}\" ${slackHook}")
 
         } else {
             script.sh("""
