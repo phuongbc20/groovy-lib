17c503b7b0e6567c80d36ae66d5b0a3aebbd71ae
admin <admin@example.com>
2023-05-08 15:46:09 +0700

    Init

--- src/com/phuongdoan/Git.groovy
+++ src/com/phuongdoan/Git.groovy
@@ -9,7 +9,7 @@ class Git implements Serializable {
     }
 
     def checkout(String repo) {
-        this.script.git credentialsId: Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "https://github.com/phuongbc20/${repo}.git", branch: "main"
+        this.script.git credentialsId: Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "http://gerrit-phuong.stg.cdgfossil.com/a/${repo}", branch: "main"
     }
 
     String commitHash() {
