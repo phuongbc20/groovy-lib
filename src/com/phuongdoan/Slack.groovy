6e9cda6e7265819d203c9f37fd1faf95fe32a2cd
admin <admin@example.com>
2023-05-08 17:31:25 +0700

    Init

--- src/com/phuongdoan/Slack.groovy
+++ src/com/phuongdoan/Slack.groovy
@@ -13,13 +13,30 @@ class Slack implements Serializable {
     void post(String status) {
         if (status == "SUCCESS") {
             script.sh("echo ${status}")
-            """
-            )
+            // script.sh("""
+            //     echo ${status}
+            //     export DATE=`TZ=":Asia/Ho_Chi_Minh" date "+%Y-%m-%d %T"`
+            //     export JSON=`jq --null-input \
+            //     --arg text "${script.env.BUILD_URL}" \
+            //     --arg color "#4BB543" \
+            //     --arg title "${script.env.JOB_NAME} |" \
+            //     '{"attachments": [ { "text": $text, "color": $color, "title": $title } ] }'`
+            //     curl -H "Content-Type:application/json" -X POST --data "${JSON}" ${slackHook}
+            // """
+            // )
         } else {
-            script.sh("""
-                script.sh("echo ${status}")
-            """
-            )
+            script.sh("echo ${status}")
+            // script.sh("""
+            //     echo ${status}
+            //     export DATE=`TZ=":Asia/Ho_Chi_Minh" date "+%Y-%m-%d %T"`
+            //     export JSON=`jq --null-input \
+            //     --arg text "${script.env.BUILD_URL}" \
+            //     --arg color "#FF0000" \
+            //     --arg title "${script.env.JOB_NAME} |}" \
+            //     '{"attachments": [ { "text": $text, "color": $color, "title": $title } ] }'`
+            //     curl -H "Content-Type:application/json" -X POST --data "${JSON}" ${slackHook}
+            // """
+            // )
         }
     }
 
