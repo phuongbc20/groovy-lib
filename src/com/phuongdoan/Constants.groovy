b7d29dd9ee416706da75ca2dee77afb18ff2be59
admin <admin@example.com>
2023-05-09 10:49:37 +0700

    Init

--- src/com/phuongdoan/Constants.groovy
+++ src/com/phuongdoan/Constants.groovy
@@ -3,5 +3,5 @@ package com.phuongdoan
 class Constants {
     public static final String SLACK_MESSAGE_HEADER = "Sending Slack Notification"
     public static final String EMAIL_MESSAGE_HEADER = "Sending Email"
-    public static final String JENKINS_GITHUB_CREDENTIALS_ID = "gerrit-viewer"
+    public static final String JENKINS_GITHUB_CREDENTIALS_ID = "gerrit-viewe"
 }
